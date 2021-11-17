a2bae1733be899b359a7a2863688beac1832d991
Sagar Parmar <sagarparmar0233@gmail.com>
2021-11-17 14:37:02 -0600

    added pages as well

--- reactwithtailwind/src/App.js
+++ reactwithtailwind/src/App.js
@@ -1,15 +1,47 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import Content from "./components/Content";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
+import Home from "./pages";
+import About from "./pages/about";
+import Menu from "./pages/menu";
+import { Routes, Route } from "react-router-dom";
+import DropDown from "./components/DropDown";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log(" i resizzedd");
+      }
+    };
+
+    window.addEventListener("resize", hideMenu);
+
+    return () => {
+      window.removeEventListener("resize", hideMenu);
+    };
+  });
   return (
     <>
-      <NavBar />
-      <Hero />
-      <Content />
+      <NavBar toggle={toggle} />
+      <DropDown isOpen={isOpen} toggle={toggle} />
+      <Routes>
+        <Route path="/" exact element={<Home />}>
+          Home
+        </Route>
+        <Route path="/menu" element={<Menu />}></Route>
+        <Route path="/about" element={<About />}>
+          About
+        </Route>
+      </Routes>
       <Footer />
     </>
   );
